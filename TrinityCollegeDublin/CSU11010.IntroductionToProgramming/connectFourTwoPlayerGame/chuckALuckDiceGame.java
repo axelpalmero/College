f7944bb3e2876b0df94c97e60048f16dc3b418ce
axelpalmero <143955322+axelpalmero@users.noreply.github.com>
2023-11-14 13:56:27 -0500

    Update chuckALuckDiceGame.java

--- TrinityCollegeDublin/CSU11010.IntroductionToProgramming/connectFourTwoPlayerGame/chuckALuckDiceGame.java
+++ TrinityCollegeDublin/CSU11010.IntroductionToProgramming/connectFourTwoPlayerGame/chuckALuckDiceGame.java
@@ -1,33 +1,4 @@
-/* SELF ASSESSMENT 
 
-1. ResolveBet
-I have correctly defined ResolveBet which takes the bet type (String) and the Wallet object, and a void return type [Mark out of 7: 7].
-Comment:I have correctly defined ResolveBet which takes the bet type (String) and the Wallet object, and a void return type
-My program presents the amount of cash in the wallet and asks the user how much he/she would like to bet [Mark out of 8: 8].
-Comment:My program presents the amount of cash in the wallet and asks the user how much he/she would like to bet
-My program ensures the bet amount is not greater than the cash in the wallet [Mark out of 5: 5].
-Comment:My program ensures the bet amount is not greater than the cash in the wallet
-My program creates three Dice objects, rolls them and creates a total variable with a summation of the roll values returned [Mark out of 15: 15]..
-Comment:My program creates three Dice objects, rolls them and creates a total variable with a summation of the roll values returned
-My program determines the winnings by comparing the bet type with the total and comparing the bet type with the dice faces for the triple bet [Mark out of 20: 20 ].
-Comment:My program determines the winnings by comparing the bet type with the total and comparing the bet type with the dice faces for the triple bet 
-My program outputs the results (win or loss) and adds the winnings to the wallet if user wins or removes the bet amount from the wallet if the user loses [Mark out of 10: 10].
-Comment: My program outputs the results (win or loss) and adds the winnings to the wallet if user wins or removes the bet amount from the wallet if the user loses
-
-2. Main
-I ask the user for the amount of cash he/she has, create a Wallet object and put this cash into it [Mark out of 15:15 ]
-Comment: I ask the user for the amount of cash he/she has, create a Wallet object and put this cash into it
-My program loops continuously until the user either enters quit or the cash in the wallet is 0 [Mark out of 5: 5]
-Comment:My program loops continuously until the user either enters quit or the cash in the wallet is 0
-I ask the user to enter any of the four bet types or quit [Mark out of 5: 5].
-Comment: I ask the user to enter any of the four bet types or quit
-My program calls resolveBet for each bet type entered [Mark out of 5:5 ].
-Comment:My program calls resolveBet for each bet type entered
-At the end of the game my program presents a summary message regarding winnings and losses [Mark out of 5:5 ]
-Comment:At the end of the game my program presents a summary message regarding winnings and losses 
-
- Total Mark out of 100 (Add all the previous marks):100
-*/
 import java.util.Scanner;
 
 public class ChuckaLuckDiceGame {
